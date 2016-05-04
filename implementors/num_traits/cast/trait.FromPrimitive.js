d17cd4fe5533c6a43a697da648021e78624c1cae
Tim Ryan <tim@3drobotics.com>
2016-05-04 10:57:23 -0700

    Adds docs.

---
+++ implementors/num_traits/cast/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['num_traits'] = [];implementors['num_bigint'] = ["impl <a class='trait' href='num_traits/cast/trait.FromPrimitive.html' title='num_traits::cast::FromPrimitive'>FromPrimitive</a> for <a class='struct' href='num_bigint/struct.BigUint.html' title='num_bigint::BigUint'>BigUint</a>","impl <a class='trait' href='num_traits/cast/trait.FromPrimitive.html' title='num_traits::cast::FromPrimitive'>FromPrimitive</a> for <a class='struct' href='num_bigint/struct.BigInt.html' title='num_bigint::BigInt'>BigInt</a>",];implementors['ptp'] = ["impl <a class='trait' href='num_traits/cast/trait.FromPrimitive.html' title='num_traits::cast::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='ptp/ptp/enum.PtpContainerType.html' title='ptp::ptp::PtpContainerType'>PtpContainerType</a>","impl <a class='trait' href='num_traits/cast/trait.FromPrimitive.html' title='num_traits::cast::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='ptp/ptp/enum.StandardCommandCode.html' title='ptp::ptp::StandardCommandCode'>StandardCommandCode</a>","impl <a class='trait' href='num_traits/cast/trait.FromPrimitive.html' title='num_traits::cast::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='ptp/ptp/enum.PtpResponseCode.html' title='ptp::ptp::PtpResponseCode'>PtpResponseCode</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
